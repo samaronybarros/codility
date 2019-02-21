cb87ee4db32a92ed53bfd96d2b141292e705f12f
Sam Barros <samarony.barros@gmail.com>
2019-02-21 22:37:25 +0100

    PermMissingElement

---
+++ Lesson 3 - Time Complexity/PermMissingElement.js
@@ -0,0 +1,54 @@
+/**
+An array A consisting of N different integers is given. The array contains integers in the range [1..(N + 1)], which means that exactly one element is missing.
+
+Your goal is to find that missing element.
+
+Write a function:
+
+function solution(A);
+
+that, given an array A, returns the value of the missing element.
+
+For example, given array A such that:
+
+  A[0] = 2
+  A[1] = 3
+  A[2] = 1
+  A[3] = 5
+the function should return 4, as it is the missing element.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [0..100,000];
+the elements of A are all distinct;
+each element of array A is an integer within the range [1..(N + 1)].
+
+-------------------------------------
+Solution: https://app.codility.com/demo/results/trainingRJ6P5D-PD4/
+Task Score: 100%
+Correctness: 100%
+Performance: 100%
+ */
+
+function solution (A) {
+  const sumWithElem = gauss(A)
+  const sumWithoutElem = sumArrElem(A)
+
+  return parseInt(sumWithElem - sumWithoutElem, 10)
+}
+
+function sumArrElem (A) {
+  let sum = 0
+
+  for (let i = 0; i < A.length; i++) {
+    sum += A[i]
+  }
+
+  return sum
+}
+
+function gauss (A) {
+  const N = A.length + 1
+
+  return (N * (N + 1)) / 2
+}
