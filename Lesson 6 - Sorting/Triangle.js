45b49f80b254f1dae297c916e9f2ff85a88a7c8c
Sam Barros <samarony.barros@gmail.com>
2019-02-22 09:43:56 +0100

    Triangle

---
+++ Lesson 6 - Sorting/Triangle.js
@@ -0,0 +1,61 @@
+/*
+An array A consisting of N integers is given. A triplet (P, Q, R) is triangular if 0 ≤ P < Q < R < N and:
+
+A[P] + A[Q] > A[R],
+A[Q] + A[R] > A[P],
+A[R] + A[P] > A[Q].
+For example, consider array A such that:
+
+  A[0] = 10    A[1] = 2    A[2] = 5
+  A[3] = 1     A[4] = 8    A[5] = 20
+Triplet (0, 2, 4) is triangular.
+
+Write a function:
+
+function solution(A);
+
+that, given an array A consisting of N integers, returns 1 if there exists a triangular triplet for this array and returns 0 otherwise.
+
+For example, given array A such that:
+
+  A[0] = 10    A[1] = 2    A[2] = 5
+  A[3] = 1     A[4] = 8    A[5] = 20
+the function should return 1, as explained above. Given array A such that:
+
+  A[0] = 10    A[1] = 50    A[2] = 5
+  A[3] = 1
+the function should return 0.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [0..100,000];
+each element of array A is an integer within the range [−2,147,483,648..2,147,483,647].
+
+-------------------------------------
+Solution: https://app.codility.com/demo/results/trainingXHMKQW-PMN/
+Task Score: 100%
+Correctness: 100%
+Performance: 100%
+*/
+
+function solution (A) {
+  if (A.length === 0) {
+    return 0
+  }
+
+  A.sort((a, b) => a - b)
+
+  let len = A.length
+
+  for (i = 0; i < len - 2; i++) {
+    let P = i + 0
+    let Q = i + 1
+    let R = i + 2
+
+    if (A[P] + A[Q] > A[R] && A[Q] + A[R] > A[P] && A[R] + A[P] > A[Q]) {
+      return 1
+    }
+  }
+
+  return 0
+}
