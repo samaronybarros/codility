81ec5300aaca9da1216635602a73150ba89f2cd1
Sam Barros <samarony.barros@gmail.com>
2019-02-25 21:35:31 +0100

    Brackets

---
+++ Lesson 7 - Stacks ans Queues/Brackets.js
@@ -0,0 +1,61 @@
+/*
+A string S consisting of N characters is considered to be properly nested if any of the following conditions is true:
+
+S is empty;
+S has the form "(U)" or "[U]" or "{U}" where U is a properly nested string;
+S has the form "VW" where V and W are properly nested strings.
+For example, the string "{[()()]}" is properly nested but "([)()]" is not.
+
+Write a function:
+
+function solution(S);
+
+that, given a string S consisting of N characters, returns 1 if S is properly nested and 0 otherwise.
+
+For example, given S = "{[()()]}", the function should return 1 and given S = "([)()]", the function should return 0, as explained above.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [0..200,000];
+string S consists only of the following characters: "(", "{", "[", "]", "}" and/or ")".
+
+-------------------------------------
+Solution: https://app.codility.com/demo/results/trainingKHYDD6-PAF/
+Task Score: 100%
+Correctness: 100%
+Performance: 100%
+*/
+
+function solution(S) {
+  if (S.length % 2 === 1) return 0
+
+  let stack = []
+  for (let value of S) {
+    if (isOpened(value)) {
+      stack.push(value)
+    } else {
+      let opened = stack.pop()
+      if (!isPair(opened, value)) {
+        return 0
+      }
+    }
+  }
+
+  if (stack.length === 0) {
+    return 1
+  }
+
+  return 0
+}
+
+function isOpened(value) {
+  return value === '{' || value === '[' || value === '('
+}
+
+function isPair(open, close) {
+  return (
+    (open === '{' && close === '}') ||
+    (open === '[' && close === ']') ||
+    (open === '(' && close === ')')
+  )
+}
